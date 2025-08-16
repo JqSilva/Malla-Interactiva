93184b7a5f829e15d5b842e8b81b9ac15d4c42d7
Joaquin <joaquinsilvagvg@gmail.com>
2025-08-16 18:31:05 -0400

    feat: Configurar el sitio y la base en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jqsilva.github.io',
+  base: 'malla-interactiva',
   vite: {
     plugins: [tailwindcss()]
   }
