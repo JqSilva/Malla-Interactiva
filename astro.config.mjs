7884bdb57f40f1c00697f6beb974e3dce2cfdc33
Joaquin <joaquinsilvagvg@gmail.com>
2025-08-16 23:08:46 -0400

    fix: Ajustar la base de URL en la configuración de la malla y limpiar el código en Malla.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jqsilva.github.io',
-  base: '/Malla-Interactiva/',
+  base: 'Malla-Interactiva',
   vite: {
     plugins: [tailwindcss()]
   }
